c99686ab1003ba9584da712421baa601d709e4c1
Anderson <stfn-aportador@adm.bancoamazonia.sa>
2024-07-05 14:43:48 -0300

    first commit

---
+++ src/main/java/com/derson/provai/ProvAiApplication.java
@@ -0,0 +1,13 @@
+package com.derson.provai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvAiApplication.class, args);
+	}
+
+}
